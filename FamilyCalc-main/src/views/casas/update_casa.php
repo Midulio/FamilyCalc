8d034a9a806791dd9ce3230ecb3c7981398ffd0e
joaquingil15 <joaquingil9308@gmail.com>
2025-06-30 20:30:53 -0300

    Add files via upload

--- FamilyCalc-main/src/views/casas/update_casa.php
+++ FamilyCalc-main/src/views/casas/update_casa.php
@@ -1,28 +1,48 @@
 <?php
 include("../../../conexion.php");
 
-if (isset($_POST['id_casa'])) {
-    $id_casa = $_POST['id_casa'];
-    $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
-    $calle = mysqli_real_escape_string($conexion, $_POST['Calle']);
-    $numero = (int) $_POST['Numero'];
-    $localidad = mysqli_real_escape_string($conexion, $_POST['Localidad']);
-    $provincia = mysqli_real_escape_string($conexion, $_POST['Provincia']);
 
-    $sql = "UPDATE casa SET 
-        nombre = '$nombre',
-        Calle = '$calle',
-        Numero = $numero,
-        Localidad = '$localidad',
-        Provincia = '$provincia'
-        WHERE id_casa = $id_casa";
+$datos = null;
 
-    if (mysqli_query($conexion, $sql)) {
-        echo "✅ Casa actualizada con éxito. <a href='editar_casa.php'>Volver</a>";
-    } else {
-        echo "❌ Error al actualizar: " . mysqli_error($conexion);
+if (isset($_GET['upd'])) {
+    $id = $_GET['upd'];
+
+    $sql = "SELECT * FROM casa WHERE id_casa = ?";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+    $resultado = $stmt->get_result();
+    if ($resultado->num_rows > 0) {
+        $datos = $resultado->fetch_assoc();
     }
-} else {
-    echo "ID de casa no recibido.";
 }
 ?>
+
+<div class="container m-auto">
+    <div class="my-3 text-center d-flex flex-row justify-content-center">
+        <p><h1>UPDATE CASA</h1></p>
+    </div>
+    <form action="upd_casa.php" method="post">
+        <div class="form-group">
+            <input type="hidden" name="id_casa" value="<?= $datos['id_casa'] ?>">
+            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="nombre" value="<?= $datos['nombre'] ?>">
+        </div>
+        <div class="form-group">
+            <input type="text" name="calle" id="calle" class="form-control" placeholder="calle" value="<?= $datos['calle'] ?>">
+        </div>
+        <div class="form-group">
+            <input type="number" name="numero" id="numero" class="form-control" placeholder="numero" value="<?= $datos['numero'] ?>">
+        </div>
+        <div class="form-group">
+            <input type="text" name="localidad" id="localidad" class="form-control" placeholder="localidad" value="<?= $datos['localidad'] ?>">
+        </div>
+        <div class="form-group">
+            <input type="text" name="provincia" id="provincia" class="form-control" placeholder="provincia" value="<?= $datos['provincia'] ?>">
+        </div>
+        <input type="submit" value="Actualizar" class="btn btn-success">
+    </form>
+</div>
+
+
+

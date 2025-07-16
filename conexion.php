45ff6ff1ee7cb1593fd73837d111317b3d9e740b
Bau23Deg <bautistanazareno07@gmail.com>
2025-07-16 16:19:19 -0300

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$pass = "";
-$base = "familycalc"; // Cambiá por el nombre real
-
-$conexion = mysqli_connect($servidor, $usuario, $pass, $base) or die("Error de conexión");
-?>
